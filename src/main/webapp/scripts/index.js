79ffcb3c99ce74a25598bcd0172a52aa4bfe33bc
Gteixeirinha7 <gabrielteixeir137@gmail.com>
2021-03-25 23:27:53 -0300

    add Key

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -1,1 +1,35 @@
-!function(){var n=angular.module("app",[]);n.controller("ItemController",["$scope","$http",function(n,e){var o=this;o.key="",o.url="",o.callPageGet=function(){var n={Key:o.key};e.get("https://consulting-page.herokuapp.com/connection",n,{method:"GET",headers:{"Content-Type":"application/json"}}).then(function(n){o.handleGET(n)},function(n){console.log(n)})},o.handleGET=function(n){o.url=n.data.objectData.urlLogin,document.getElementById("myIframe").src=o.url.replace("pageid=","pageid="+window.location.search.split("?pageId=")[1])},o.init=function(){o.callPageGet()}}]),n.config(["$qProvider",function(n){n.errorOnUnhandledRejections(!1)}])}();
+(function () {
+var app = angular.module('app', []);
+
+app.controller('ItemController', ['$scope', '$http', function (scope, $http) {
+    var c = this;
+
+    c.callPageGet = function () {
+        var req = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        $http.get('https://consulting-page.herokuapp.com/connection', body, req).then(
+            function successCallback(response) { 
+                debugger;
+                c.handleGET(response);
+            }, 
+            function errorCallback(response) { 
+                console.log(response); 
+            }
+        );
+    };
+    c.handleGET = function (response) {
+        document.getElementById('myIframe').src = response.data.objectData.urlLogin.replace('pageid=', 'pageid='+window.location.search.split('?pageId=')[1]);
+
+    };
+    c.init = function () {
+        c.callPageGet();
+    };
+}]);
+app.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);
+})();
